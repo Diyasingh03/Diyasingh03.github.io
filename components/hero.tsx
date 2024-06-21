8af15e3fd4114cc6c4278493a98f509aa8dcd4b0
Diya <singhdiya2004@gmail.com>
2024-06-21 07:47:47 -0400

    switched to nextjs

---
+++ components/hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+        <div>
+            
+        </div>
+    </div>
+
+  )
+}
+
+export default hero
