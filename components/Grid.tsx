f75c4f2b46d6dbf01ba65f0c9a32e005e49ba4cc
Diya <singhdiya2004@gmail.com>
2024-06-26 02:19:46 -0400

    Grid outline created

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
     <section id="about">
-        <BentoGrid>
-            {[{title:'Title1', description:'Desc1' , id:1}].map ((item) => (
-                <BentoGridItem id={item.id} key={item.id} title={item.title} description={item.description} />
-            ))}
-        </BentoGrid>
+      <BentoGrid>
+        {gridItems.map(({id,title,description,className,img,imgClassName,titleClassName,spareImg}) => (
+          <BentoGridItem
+            id={id}
+            key={id} 
+            title={title} 
+            className={className}
+            img={img}
+            imgClassName={imgClassName}
+            titleClassName={titleClassName}
+            spareImg={spareImg}
+            description={description} />
+        ))}
+      </BentoGrid>
     </section>
   )
 }
